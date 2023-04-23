22adea26f3b709e48d793c3bd365f9227522bdc0
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-04-23 10:38:12 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -17,8 +17,11 @@ typedef struct listint_s
         struct listint_s *next;
 } listint_t;
 
+/*Given printing functions*/
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+/*Sorting algorithms*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
