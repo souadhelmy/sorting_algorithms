430973814b3193237edd89a539d52bac820b8296
souadhelmy <118821486+souadhelmy@users.noreply.github.com>
2023-04-23 12:51:01 +0200

    Update sort.h

--- sort.h
+++ sort.h
@@ -3,6 +3,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -12,16 +26,13 @@
  */
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/*Given printing functions*/
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/*Sorting algorithms*/
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
@@ -36,4 +47,3 @@ void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
 #endif
-
